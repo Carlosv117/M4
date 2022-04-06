d28edb357ba5b88c6974c8e9f5c075cf6f479197
carlosv117 <limacarlosvitor@gmail.com>
2022-04-06 16:33:36 -0300

    Aula 4

--- Atividades Kenzie Sprint-1/Atividade 3 - Primeiros passos com Express/src/app.js
+++ Atividades Kenzie Sprint-1/Atividade 3 - Primeiros passos com Express/src/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("App rodando");
+  res.send("Hello Word!!!");
 });
 
 app.listen(port, () => {
